550b16443e980ae58e642bf3551bae1a1d6b9d40
utiyong <utibe.ekpenyong@stu.cu.edu.ng>
2026-06-05 11:36:48 +0100

    added the url to be used by render

--- documentation.js
+++ documentation.js
@@ -2,7 +2,7 @@ const swagger = require('swagger-jsdoc');
 
 const options = {
     apis: [
-        "./docs/farmer.yaml"
+        "./docs/farmer.yaml", "./docs/driver.yaml", 
     ],
     
     definition: {
--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use('/apisDocs', swaggerUi.serve, swaggerUi.setup(swagger))
+app.use('/api/v1/apiDocs', swaggerUi.serve, swaggerUi.setup(swagger))
 
 
 
