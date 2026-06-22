ac3bfc55df571ac1992117c1ba4062f0c6aac0f9
utiyong <utibe.ekpenyong@stu.cu.edu.ng>
2026-06-22 18:58:08 +0100

    I updated the withdraw to use bank Code instead of manually inputing it

---
+++ utils/bankNamesAndCodes.js
@@ -0,0 +1,20 @@
+// utils/nigerianBanks.js
+
+exports.nigerianBanks = [
+  { bankCode: '044', bankName: 'Access Bank' },
+  { bankCode: '050', bankName: 'Ecobank Nigeria' },
+  { bankCode: '070', bankName: 'Fidelity Bank'},
+  { bankCode: '011', bankName: 'First Bank of Nigeria' },
+  { bankCode: '214', bankName: 'First City Monument Bank' },
+  { bankCode: '058', bankName: 'Guaranty Trust Bank Plc' },
+  { bankCode: '082', bankName: 'Keystone Bank Ltd' },
+  { bankCode: '076', bankName: 'Polaris Bank' },
+  { bankCode: '039', bankName: 'Stanbic IBTC' },
+  { bankCode: '232', bankName: 'Sterling Bank' },
+  { bankCode: '032', bankName: 'Union Bank Nigeria' },
+  { bankCode: '033', bankName: 'United Bank for Africa' },
+  { bankCode: '215', bankName: 'Unity Bank' },
+  { bankCode: '035', bankName: 'WEMA Bank' },
+  { bankCode: '057', bankName: 'Zenith Bank' }
+]
+
