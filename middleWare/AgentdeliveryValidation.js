aed393a9da1b3f37340d40dfe083cf11152f43e8
utiyong <utibe.ekpenyong@stu.cu.edu.ng>
2026-06-24 02:48:23 +0100

    I have updated the code to allow validations for most input sections

--- middleWare/AgentdeliveryValidation.js
+++ middleWare/AgentdeliveryValidation.js
@@ -1,8 +1,12 @@
 const joi = require('joi');
 
 
-exports.createAgentDelivery = (req, res, next) => {
+exports.createAgentDeliveryValidation = (req, res, next) => {
     const schema = joi.object({
+        agentFarmerId: joi.string().required().messages({
+            'any.required': 'Farmer selection is required',
+            'string.empty': 'Farmer selection cannot be empty'
+        }),
         produceType: joi.string().trim().required().messages({
             'any.required': 'Produce type is required',
             'string.empty': 'Produce type cannot be empty'
@@ -28,6 +32,54 @@ exports.createAgentDelivery = (req, res, next) => {
             'any.required': 'Customer Name is required',
             'string.empty': 'Customer Name cannot be empty'
         }),
+    })
+
+    const { error } = schema.validate(req.body)
+    if(error) {
+        return res.status(400).json({
+            message: error.details[0].message
+        })
+    }
+
+    next()
+}
+
+
+exports.createAgentKycValidation = (req, res, next) =>{
+     const schema = joi.object({
+        state: joi.string().trim().required().messages({
+            'any.required': 'Agents State is required',
+            'string.empty': 'Agents State cannot be empty'
+        }),
+        residentialAddress: joi.string().trim().required().messages({
+            'any.required': 'Agents Residential Address is required',
+            'string.empty': 'Agents Residential Address cannot be empty'
+        }),
+        kinsFirstName: joi.string().trim().required().messages({
+            'any.required': 'Ageents kins first Name location is required',
+            'string.empty': 'Ageents kins first Name location cannot be empty'
+        }),
+        kinsLastName: joi.string().trim().required().messages({
+            'any.required': 'Agents kins last Name is required',
+            'string.empty': 'Agents kins last Name cannot be empty'
+        }),
+        kinsPhoneNumber: joi.string().pattern(/^\d{11}$/).required().messages({
+            'any.required': 'Agents kins phone number is required',
+            'string.empty': 'Agents kins phone number cannot be empty',
+            'string.pattern.base': 'Agents kins phone number must be 11 digits'
+        }),
+        kinsEmail:joi.string().email().required().messages({
+                    'string.email': 'Agents kins email must be valid',
+                    "string.empty": "Agents kins email cannot be empty",
+         }),
+         kinsRelationship:joi.string().required().messages({
+                    "string.empty": "Agents kins Relationship cannot be empty",
+         }),
+         kinsLgaOrTown:joi.string().trim().required().messages({
+            'any.required': 'Agents kins last Name is required',
+            'string.empty': 'Agents kins last Name cannot be empty'
+        }),
+                
     })
 
     const { error } = schema.validate(req.body)
